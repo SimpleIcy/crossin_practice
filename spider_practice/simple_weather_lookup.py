0d02bf9bedf58bd4b2060f5e65ba116398a4f911
sampleicy <364027865@qq.com>
2020-03-11 22:20:55 +0800

    spider practice 1

---
+++ spider_practice/simple_weather_lookup.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+
+while True:
+    city = input('输入要查询的城市：')
+    if city:
+        info = requests.get('http://wthrcdn.etouch.cn/weather_mini?city=%s' % city)
+        weather = json.loads(info.text)
+        if weather.get('desc') == 'OK':
+            date = weather.get('data').get('forecast')[0].get('date')
+            high_temperature = weather.get('data').get('forecast')[0].get('high')
+            low_temperature = weather.get('data').get('forecast')[0].get('low')
+            weather_type = weather.get('data').get('forecast')[0].get('type')
+            print('%s\n%s\n%s\n%s' % (date, high_temperature, low_temperature, weather_type))
+        else:
+            print('未获得城市')
+            continue
+    else:
+        break
