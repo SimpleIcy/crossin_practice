c1199cf7fe2b5091caa07405e84f8e537c36619d
simpleIcy <d360478265@163.com>
2020-03-12 20:33:22 +0800

    Update simple_weather_lookup.py
    change str to dict.

--- spider_practice/simple_weather_lookup.py
+++ spider_practice/simple_weather_lookup.py
@@ -1,12 +1,11 @@
 import requests
-import json
 
 
 while True:
     city = input('输入要查询的城市：')
     if city:
         info = requests.get('http://wthrcdn.etouch.cn/weather_mini?city=%s' % city)
-        weather = json.loads(info.text)
+        weather = info.json()
         if weather.get('desc') == 'OK':
             date = weather.get('data').get('forecast')[0].get('date')
             high_temperature = weather.get('data').get('forecast')[0].get('high')
